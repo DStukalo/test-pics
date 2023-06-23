f23260e148ccb0fb2a7b4815db90227e423a4db5
DStukalo <94228608+DStukalo@users.noreply.github.com>
2023-06-23 16:54:47 +0300

    feat : add form add comment

---
+++ test-pics/src/components/FormAddComment/index.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from 'react';
+import { generateRandomNumber } from '../../helpers/funcGenerateRandomNumber';
+import { getValueFromStorage } from '../../helpers/getValueFromStorage';
+import styles from './FormAddComment.module.scss';
+import { setValueToStorage } from '../../helpers/setValueToStorage';
+import type { CommentType } from '../../interfaces/interfaces';
+
+interface Props {
+	comments: CommentType[];
+	setComments: (arg0: CommentType[]) => void;
+	setLoading: (arg0: boolean) => void;
+	setError: (arg0: boolean) => void;
+}
+
+export function FormAddComment({
+	comments, setComments, setLoading, setError,
+}: Props) {
+	const [textareaValue, setTextareaValue] = useState<string | null>(getValueFromStorage('textareaValue'));
+
+	useEffect(() => {
+		const storedValue = getValueFromStorage('textareaValue');
+		if (storedValue) {
+			setTextareaValue(storedValue);
+		}
+	}, []);
+
+	const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+		const { value } = event.target;
+		setTextareaValue(value);
+		setValueToStorage('textareaValue', value);
+	};
+
+	async function createNewComment() {
+		const baseUrl = import.meta.env.VITE_BASE_URL;
+		const text = textareaValue;
+		setValueToStorage('textareaValue', '');
+		setLoading(true);
+		try {
+			const dataFetch = await fetch(`${baseUrl}add`, {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({
+					body: `${text}`,
+					postId: generateRandomNumber(),
+					userId: generateRandomNumber(),
+				}),
+			});
+			const newComment: CommentType = await dataFetch.json();
+			const newArray = [...comments, newComment];
+			setComments(newArray);
+		} catch (error) {
+			setError(true);
+		} finally {
+			setLoading(false);
+		}
+	}
+	const onBtnClick = () => {
+		createNewComment();
+	};
+
+	return (
+		<form className={styles.form}>
+			<textarea value={textareaValue || ''} name="addForm" className={styles.area} onChange={handleChange} />
+			<button type="button" onClick={onBtnClick} className={styles.btn}>Send</button>
+		</form>
+	);
+}
