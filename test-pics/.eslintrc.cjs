3aa2fcb693ad432925bb33761fb88835ed74528b
DStukalo <94228608+DStukalo@users.noreply.github.com>
2023-06-23 16:49:43 +0300

    refactor: update eslint config

--- test-pics/.eslintrc.cjs
+++ test-pics/.eslintrc.cjs
@@ -9,7 +9,9 @@ module.exports = {
 		"airbnb/hooks",
 		"eslint:recommended",
 		"plugin:react/recommended",
+		"plugin:@typescript-eslint/recommended",
 		"plugin:react/jsx-runtime",
+		"./.eslintrc-typescript.cjs",
 	],
 	parserOptions: {
 		ecmaFeatures: {
@@ -19,11 +21,46 @@ module.exports = {
 		sourceType: "module",
 	},
 	plugins: ["react"],
+	settings: {
+		"import/resolver": {
+			typescript: {},
+		},
+	},
 	rules: {
+		indent: [2, "tab"],
+		"react/jsx-indent": [
+			2,
+			"tab",
+			{ checkAttributes: false, indentLogicalExpressions: true },
+		],
+		"react/jsx-indent-props": [2, "tab"],
+		"no-tabs": 0,
+		"linebreak-style": [2, "unix"],
+		quotes: [2, "single"],
+		semi: [2, "always"],
+		"no-plusplus": [2, { allowForLoopAfterthoughts: true }],
 		"react/jsx-filename-extension": [
 			2,
 			{
-				extensions: [".js", "jsx"],
+				extensions: [".js", "jsx", ".ts", ".tsx"],
+			},
+		],
+		"import/prefer-default-export": "off",
+		"import/extensions": [
+			2,
+			"ignorePackages",
+			{
+				js: "never",
+				jsx: "never",
+				ts: "never",
+				tsx: "never",
+			},
+		],
+		"react/require-default-props": ["off"],
+		"react/jsx-filename-extension": [
+			2,
+			{
+				extensions: [".js", "jsx", ".ts", ".tsx"],
 			},
 		],
 		"import/extensions": [
@@ -32,20 +69,18 @@ module.exports = {
 			{
 				js: "never",
 				jsx: "never",
+				ts: "never",
+				tsx: "never",
 			},
 		],
-		"import/prefer-default-export": "off",
-		"no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
-		"no-tabs": 0,
-		"react/jsx-indent": [
-			2,
-			"tab",
-			{ checkAttributes: false, indentLogicalExpressions: true },
+		"react/no-unknown-property": [2, { ignore: ["css"] }],
+		"react/jsx-props-no-spreading": "off",
+		"no-param-reassign": [
+			"error",
+			{
+				props: true,
+				ignorePropertyModificationsFor: ["state"],
+			},
 		],
-		"react/jsx-indent-props": [2, "tab"],
-		indent: ["error", "tab"],
-		"linebreak-style": ["error", "unix"],
-		quotes: ["error", "single"],
-		semi: ["error", "always"],
 	},
 };
